5a364cd468ab4f45f623f1a51029c15a877d68c9
solarusknight <147309297+solarusknight@users.noreply.github.com>
2023-12-29 22:36:30 +0530

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,12 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const path = require('path')
 const regSchema = require('./models/registerSchema')
+const notifyAppLogin = require('./models/notifySchema')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
 mongoose.connect("mongodb+srv://solarisknight:wolf@cluster0.fjloxgx.mongodb.net/Portfolio?retryWrites=true&w=majority")
+// mongoose.connect("mongodb://localhost:27017/20MER121")
 .then(()=>{
     console.log("connected to the mongoDB Servers")
 })
@@ -13,7 +18,7 @@ mongoose.connect("mongodb+srv://solarisknight:wolf@cluster0.fjloxgx.mongodb.net/
 const App = express()
 App.use(cors())
 App.listen(process.env.PORT,()=>{
-    console.log("Node App running on port 3001")
+    console.log("Node App running")
 });
 //getting server response
 App.get("/",(req,res)=>{
@@ -26,7 +31,7 @@ App.post("/register", async(req,res)=>{
     try {
         const inputData = await regSchema.create(req.body)
         res.status(200).json(inputData)
-        console.log(inputData)
+        //console.log(inputData)
     } catch (error) {
         res.status(400).json({message:"Something went wrong"})
         console.log("Couldn't Store datas into the servers")
@@ -37,20 +42,75 @@ App.get("/get",async(req,res)=>{
     try {
         const fetchedData = await regSchema.find()
         res.status(200).json(fetchedData)
-        console.log(fetchedData)
+        //console.log(fetchedData)
     } catch (error) {
         res.status(400).json({message:"Couldn't fetch datas from the server"})
         console.log("Couldn't fetch datas from the server")
     }
 })
-
-App.post("/login",async(req,res)=>{
-    try {
-        const EnteredUname = req.body.username;
-        const EnteredPassword = req.body.password;
-        const User = await regSchema.findOne({username:EnteredUname})
-        EnteredUname===User.username && EnteredPassword===User.password ? res.status(200).json({respond:"Approve"}) : res.status(400).json({respond:"Prevent"})
-    } catch (error) {
-        console.log("Node Error")
+App.post('/createuser',async(req,res)=>{
+    const {username} = req.body
+    const {password} = req.body
+    const hashed_password = await bcrypt.hash(password,10)
+    try{
+        const data = await notifyAppLogin.create({"username":username,"password":hashed_password})
+        res.status(200).json(data)
+    }
+    catch(err){
+        console.log("Something went wrong")
+        res.status(500).json({"Message":"Something went wrong"})
+    }
+})
+//Login JWT
+App.post("/generateToken",async(req,res)=>{
+    const {username} = req.body
+    const {password} = req.body
+    try{
+        const obj = await notifyAppLogin.find({"username":username})
+        if(obj.length > 1){
+            return res.json({message:"Something went wrong!"})
+            // return res.status(404).send("Something went wrong")
+        }
+        else if(obj.length <1){
+            // return res.status(404).json({message:"User not found"})
+             return res.json({message:"User not found"})
+        }
+        var Password_Server
+        obj.map((o)=>{
+            Password_Server = o.password
+        })
+        const password_verify = await bcrypt.compare(password,Password_Server)
+        var token
+        if(password_verify){
+            const secret = process.env.SECRET_KEY
+            token = jwt.sign(username,secret)
+            return res.json({token:token})
+        }
+        else{
+            return res.json({message:"Invalid password"})
+        }
+    }
+    catch(e){
+        // res.status(404).json({message:"Something went wrong"})
+        res.json({message:"Something went wrong"})
+    }
+})
+//get loginData JWT
+App.get("/getToken",(req,res)=>{
+    const Header = process.env.HEADER_NAME
+    const secret = process.env.SECRET_KEY
+    try{
+        const token = req.header(Header)
+        if(token){
+            const data = jwt.verify(token,secret)
+            // console.log(data)
+            res.send(data)
+        }
+        else{
+            res.send("Invalid token")
+        }
+    }
+    catch(err){
+        console.log(err)
     }
 })
